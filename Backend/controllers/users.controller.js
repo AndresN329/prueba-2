080342331f6b87f4ddd00cf72249045f31200c9c
AndresN329 <andresnieblesmartinez@gmail.com>
2025-08-29 08:16:34 -0500

    Add files via upload

---
+++ Backend/controllers/users.controller.js
@@ -0,0 +1,101 @@
+const client = require('../models/users.model');
+const { comparePassword } = require('../utils/auth.utils');
+const generalClient = require('../models/general.model')
+
+
+exports.getUsers = (req,res)=>{
+    client.getAll((err, results) =>{
+        if (err) throw err
+        res.json(results);
+    });
+};
+
+exports.getUser = (req, res) =>{
+    const id = req.params.id;
+    client.getOne(id,(err, results) => {
+        if (err) throw err;
+        res.json(results)
+    })
+}
+
+exports.updateCoders = (req, res) => {
+  const data = req.body;
+  const id = req.params.id;
+  client.update(id, data, (err) =>{
+    if (err) return res.status(500).json({error : err.message});
+    res.json({message : 'Informacion actualizada exitosamente'});
+  });
+};
+
+exports.changePassword = async(req, res) =>{
+  const id = req.params.id;
+  const {password, newPassword} = req.body;
+  client.getOne(id, async (err, results) => {
+    if (err) {
+      return res.status(500).json({ message: "Error interno del servidor" });
+    }
+
+    if (!results || results.length === 0) {
+      return res.status(401).json({ message: "Credenciales inválidas (email)" });
+    }
+
+    const user = results[0];
+    if (!user) {
+      return res.status(401).json({ message: "Credenciales inválidas (email)" });
+    }
+    const validPassword = await comparePassword(password, user.password);
+    if (!validPassword) return res.status(401).json({ message: "Credenciales inválidas (password)" });
+    client.changePassword(id, newPassword, (err) =>{
+      if (err) res.status(500).json({error : err.message});
+      res.json({message : 'Contraseña actualizada con exito'})
+    })
+  });
+  
+};
+
+
+exports.updateAdmin = async (req,res) => {
+  const id = req.params.id;
+  const data = req.body;
+  generalClient.cityId(data, (err, cityResults) => {
+      if (err) return res.status(500).json({ error: err.message });
+      if (cityResults.length === 0) return res.status(404).json({ error: "Ciudad no encontrada" });
+  
+      const placeId = cityResults[0].id;
+  
+    
+      generalClient.clanId(data, (err, clanResults) => {
+        if (err) return res.status(500).json({ error: err.message });
+        if (clanResults.length === 0) return res.status(404).json({ error: "Clan no encontrado" });
+  
+        const clanId = clanResults[0].id;
+                const modifiedData = {
+                  place_id : placeId,
+                  clan_id : clanId
+                };
+                client.adminUpdate(id, data, modifiedData, (err, result) =>{
+                if (err) return res.status(500).json({error: err.message});
+                res.json({message : "Usuario actualizado con exito"});
+              });
+            }
+          );
+        }
+      );
+    };
+        
+
+exports.deleteUser = (req,res) =>{
+    const id = req.params.id;
+    client.delete(id, (err) =>{
+        if (err) return res.status(500).json({error : err.message});
+        res.json({message : 'Usuario eliminado correctamente'});
+    });
+};
+
+exports.showUserTeams = (req, res) => {
+    const id = req.params.id;
+    client.showUserTeams(id, (err, results) => {
+        if (err) throw (err);
+        res.json(results)
+    });
+};
